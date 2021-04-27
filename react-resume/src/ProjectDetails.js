52397b28f687d223eb4f97c0aad3f67300d019c7
Ryan Fritz <ryanfritzc@gmail.com>
2021-04-27 01:43:40 -0400

    slight formating adjustments

---
+++ react-resume/src/ProjectDetails.js
@@ -0,0 +1,34 @@
+import { useHistory, useParams } from "react-router-dom";
+import useFetch from './useFetch';
+
+const BlogProject = () => {
+    const {id} = useParams();
+    const {data: project, error, isPending} = useFetch('http://localhost:8000/blogs/'+id)
+    const history = useHistory();
+    
+    const handleClick = () =>{
+        fetch('http://localhost:8000/blogs/' + project.id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        })
+
+    }
+
+    return ( 
+        <div className ="blog-details">
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{project.title}</h2>
+                    <p>Written by {project.author}</p>
+                    <div>{project.body}</div>
+                    <button onClick={handleClick}>Delete</button>
+                </article>
+            )}
+        </div>
+     );
+}
+ 
+export default ProjectDetails;

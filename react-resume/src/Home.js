8fd07b1e89207c77dad6646ebb23f5a4f2512465
Ryan Fritz <ryanfritzc@gmail.com>
2021-04-29 15:22:12 -0400

    added an image of my face and laid the ground work for adding stuff to resume/bio page

--- react-resume/src/Home.js
+++ react-resume/src/Home.js
@@ -1,9 +1,20 @@
-import ProjectList from './ProjectList';
-import useFetch from './useFetch';
+//import ProjectList from './ProjectList';
+//import useFetch from './useFetch';
 
 const Home = () => {
     return(
-        <h1>testing one</h1>
+        <div className="home-bio">
+            <h1 className="bio-title">Ryan Fritz</h1>
+            <br/>
+            <img src={require('./goodLookin.PNG').default} />
+            <br/>
+            <div>
+                <h2>Gradutating with a 3.6 GPA in Computer Science</h2>
+            </div>
+        </div>
+        
+        
+        
     );
 }
  

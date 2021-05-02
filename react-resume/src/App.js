d5d4aba19f673a442f14ac47395dbed60d584733
Ryan Fritz <ryanfritzc@gmail.com>
2021-05-02 18:48:26 -0400

    fixing app

--- react-resume/src/App.js
+++ react-resume/src/App.js
@@ -17,7 +17,7 @@ function App() {
             <Route exact path="/">
               <Home />
             </Route>
-            <Route exact path="ryanfritzc.github.io/reactResume/">
+            <Route exact path="/reactResume/">
               <Home />
             </Route>
             <Route path="/HiddenAdd">

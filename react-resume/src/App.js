93b00cb17e9d2999c875e30325d0c2654ea5a485
Ryan Fritz <ryanfritzc@gmail.com>
2021-05-03 02:18:29 -0400

    potential

--- react-resume/src/App.js
+++ react-resume/src/App.js
@@ -14,7 +14,7 @@ function App() {
         <NavBar />
         <div className="content">
           <Switch>
-            <Route exact path="https://ryanfritzc.github.io/reactResume/">
+            <Route exact path="/">
               <Home />
             </Route>
             <Route exact path="https://ryanfritzc.github.io/reactResume/">
